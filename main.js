d2d9db0acdf9733889d6c6da88218ed811e152e2
fontcuberta <isafontcu@gmail.com>
2018-09-30 17:19:17 +0200

    Data binding

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
+
+// input.addEventListener('keyup', () =>  h1.innerHTML = input.value)
+
+new Vue({
+    el: 'main',
+    data: {
+        message: "Hola mundo"
+    }
+});
