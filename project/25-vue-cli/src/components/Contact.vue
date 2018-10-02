d00346517d551bc2d123e9d044e0c5bf7dcdcd18
fontcuberta <isafontcu@gmail.com>
2018-10-02 16:53:06 +0200

    Routes programatic navigation

--- project/25-vue-cli/src/components/Contact.vue
+++ project/25-vue-cli/src/components/Contact.vue
@@ -1,4 +1,17 @@
 <template>
-    <h2>Contact
-    </h2>
+    <div>
+        <h2>Contact</h2>
+        <button @click='backHome()'>Back Home</button>
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        backHome () {
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
