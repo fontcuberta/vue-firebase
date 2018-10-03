2975e1597a406cb2beef06269cd569cbed8d41db
fontcuberta <isafontcu@gmail.com>
2018-10-03 13:40:55 +0200

    Jest implemented for testing

---
+++ project/25-vue-cli/src/test/Task.spec.js
@@ -0,0 +1,11 @@
+//import { shallowMount } from '@vue/test-utils'
+import Task from '@/components/Task.vue'
+
+describe('Task.vue', () => {
+  it('renders li for each item in props.items', () => {
+      console.log("You fucking tester")
+
+    expect(1).toBe(1)
+  })
+
+})
